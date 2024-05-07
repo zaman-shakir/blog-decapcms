266f025a645eaa3c3c843e394fa3eeb85a03f2ee
Shakir Uz Zaman <zaman.shakirdev@gmail.com>
2024-05-07 17:10:02 +0600

    two column custom  widget testing3

--- src/routes/blog/[post]/+page.js
+++ src/routes/blog/[post]/+page.js
@@ -5,7 +5,7 @@ export const load = async ({ params }) => {
 	console.log(params);
 	try {
 		const post = await import(`../../../lib/posts/${params.post}.md`)
-
+        console.log(post);
 		return {
 			PostContent: post.default,
 			meta: { ...post.metadata, slug: params.post }
